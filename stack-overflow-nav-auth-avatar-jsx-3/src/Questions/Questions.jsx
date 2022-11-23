472a3955154db89ca628669186099faeeb363478
Sai Dhakshan Y <112693557+gmsai12345@users.noreply.github.com>
2022-11-23 09:02:15 +0530

    Update Questions.jsx

--- stack-overflow-nav-auth-avatar-jsx-3/src/Questions/Questions.jsx
+++ stack-overflow-nav-auth-avatar-jsx-3/src/Questions/Questions.jsx
@@ -1,10 +1,32 @@
 import React from "react";
-const Questions = () => {
-  return <div>
-    <div className = "display-ans-container"></div>
-    <div className = "display-votes-ans">
-      <p>{Questions.votes}</p>
+import {Link} from "react-router-dom";
+const Questions = (questtion) => {
+  return (
+    <div>
+      <div className="display-ans-container"></div>
+      <div className="display-votes-ans">
+        <p>{Questions.votes}</p>
+        <p>votes</p>
+        <div className="display-ans-container"></div>
+      <div className="display-votes-ans">
+        <p>{Questions.noOfAnswers}</p>
+        <p>answers</p>
+      </div>
+      <div className = "display-question-default">
+      <Link to = {'/QuestionList/${question.id()}'} className = "question.title">{question.questionTitle()}</Link>
+      <div className = "display-tags-time">
+      <div className = "display-tags">
+        {
+          question.tag.map((tag) =>(
+            <p key = {tag}> {tag} </p>
+          )
+          
+          )
+        }
+          <p className = "display-title"
+      </div>
+
     </div>
-  </div>;
+  );
 };
 export default Questions;
